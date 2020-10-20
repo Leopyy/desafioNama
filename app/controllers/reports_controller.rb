ed5de861f0af56cd09de2d397cef428e73c5d7bf
Leonardo Figueira <leofigueiracorreia@yahoo.com.br>
2020-10-20 17:09:48 -0300

    add parse function on the report controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,6 @@
 class ReportsController < ApplicationController
+  require 'csv'
+
   def index
     @reports = Report.all
   end
@@ -8,11 +10,21 @@ class ReportsController < ApplicationController
   end
 
   def parse
-    
-  end
+    csv_options = { col_sep: '/t', quote_char: '"', headers: :first_row }
+    filepath    = url_for(@report.file)
+    @items = []
 
+    CSV.foreach(filepath, csv_options) do |row|
+      item = { buyer: row['Comprador'], description: row['Descrição'], price: row['Preço Unitário'], quantitiy: row['Quantidade'], adress: row['Endereço'], supplyer: row['Fornecedor'] }
+      @items << item
+    end
+
+    return @items
+  end
+        
   def create
     @report = Report.new(report_params)
+    @report.parse
     if @report.save
       flash[:success] = "Arquivo enviado com sucesso"
     else
