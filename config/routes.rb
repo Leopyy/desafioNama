a6698082aff2dc4331d7b46491199eae724c3c72
Leonardo Figueira <leofigueiracorreia@yahoo.com.br>
2020-10-21 04:22:08 -0300

    add items index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :reports, only: [:create, :index, :new]
+  resources :reports, only: [:create,:new, :show]
+  resources :items, only: [:index]
 end
